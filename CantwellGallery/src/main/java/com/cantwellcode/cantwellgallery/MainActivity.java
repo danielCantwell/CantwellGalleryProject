867dca78fb77c992558c63ea253669f84ff1c370
dcantwell <dcantwel@usc.edu>
2013-08-13 13:23:47 -0700

    Made use of Fragments
    Panes are now Fragments
    MainActivity is now a FragmentActivity
    Each Pane has its own layout xml

--- CantwellGallery/src/main/java/com/cantwellcode/cantwellgallery/MainActivity.java
+++ CantwellGallery/src/main/java/com/cantwellcode/cantwellgallery/MainActivity.java
@@ -1,23 +1,62 @@
 package com.cantwellcode.cantwellgallery;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.SlidingPaneLayout;
+import android.view.Display;
 import android.view.Menu;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        final View root = getLayoutInflater().inflate(R.layout.activity_main, null);
+        setContentView(root);
+
+        final SlidingPaneLayout slidingPaneLayout = SlidingPaneLayout.class.cast(root.findViewById(R.id.slidingpanelayout));
+
+        slidingPaneLayout.setPanelSlideListener(new SlidingPaneLayout.PanelSlideListener() {
+
+            @Override
+            public void onPanelSlide(View view, float v) {
+            }
+
+            @Override
+            public void onPanelOpened(View view) {
+
+                switch (view.getId()) {
+                    case R.id.album_pane:
+                        getSupportFragmentManager().findFragmentById(R.id.photo_pane).setHasOptionsMenu(true);
+                        getSupportFragmentManager().findFragmentById(R.id.photo_pane).setHasOptionsMenu(false);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onPanelClosed(View view) {
+
+                switch (view.getId()) {
+                    case R.id.album_pane:
+                        getSupportFragmentManager().findFragmentById(R.id.photo_pane).setHasOptionsMenu(false);
+                        getSupportFragmentManager().findFragmentById(R.id.album_pane).setHasOptionsMenu(true);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
     
 }
---
+++ CantwellGallery/src/main/java/com/cantwellcode/cantwellgallery/PhotoPane.java
@@ -0,0 +1,25 @@
+package com.cantwellcode.cantwellgallery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.graphics.Color;
+
+/**
+ * Created by Daniel on 8/13/13.
+ */
+public class PhotoPane extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        final View root = inflater.inflate(R.layout.photo_pane, container, false);
+        root.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        root.setBackgroundColor(Color.CYAN);
+
+        return root;
+    }
+
+}
