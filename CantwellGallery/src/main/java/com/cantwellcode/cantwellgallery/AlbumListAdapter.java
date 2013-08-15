000c09490f23d541c20753f2fbdca85d7a133d29
dcantwell <dcantwel@usc.edu>
2013-08-15 16:57:15 -0700

    Cleaned up formatting and added comments
    I did not comment and any of the cursor stuff because I don't know what it is.

--- CantwellGallery/src/main/java/com/cantwellcode/cantwellgallery/AlbumListAdapter.java
+++ CantwellGallery/src/main/java/com/cantwellcode/cantwellgallery/AlbumListAdapter.java
@@ -16,74 +16,150 @@ import java.util.List;
  * Created by Chris on 8/14/13.
  */
 public class AlbumListAdapter extends BaseAdapter {
+
+    /**
+     * Inner class to hold the icon and text for each view in the list
+     */
     static class ViewHolder{
         ImageView icon;
-        TextView text;
+        TextView  text;
     }
-    private static final int ACTIVE_KEY = 0;
-    private static final int INACTIVE_KEY = 1;
-    private Context mContext;
-    private int mActivePosition;
-    private LayoutInflater mInflater;
-    private List<File> mAlbums;
 
+    /*
+     * Keys for setting and getting the tags
+     * of the Views in the List
+     */
+    private static final int ACTIVE_KEY   = 0;
+    private static final int INACTIVE_KEY = 1;
+
+    private int             mActivePosition;
+    private Context         mContext;
+    private LayoutInflater  mInflater;
+    private List<File>      mAlbums;
+
+    /**
+     * Constructor that does not have a List of Files as a parameter
+     * so it creates a blank list
+     * @param context
+     * @param activePosition
+     */
     public AlbumListAdapter(Context context, int activePosition){
         init(context, new ArrayList<File>(), activePosition);
     }
 
+    /**
+     * Constructor that includes the List of Files, which are the albums
+     * @param context
+     * @param albums
+     * @param activePosition
+     */
     public AlbumListAdapter(Context context, List<File> albums, int activePosition){
         init(context,albums,activePosition);
     }
 
+    /**
+     * Returns the size of the List of album Files
+     * @return
+     */
     @Override
     public int getCount() {
         return mAlbums.size();
     }
 
+    /**
+     * Returns the File at index i
+     * @param i
+     * @return
+     */
     @Override
     public Object getItem(int i) {
         return mAlbums.get(i);
     }
 
+    /**
+     * Returns the @id/ of the item at index i
+     * @param i
+     * @return
+     */
     @Override
     public long getItemId(int i) {
         return 0;
     }
 
+    /**
+     * Called each time a view needs to be loaded
+     * to be displayed in the list
+     * It controls what and how each item is displayed
+     * @param position
+     * @param convertView
+     * @param parent
+     * @return
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
         ViewHolder holder;
+
         File album = mAlbums.get(position);
+
+        // if the list view is first being populated
         if (convertView == null){
             holder = new ViewHolder();
+
+            // for the active position
             if (position==mActivePosition){
+                // inflate the view with the layout for "active position"
                 convertView=mInflater.inflate(R.layout.active_album, null, false);
-                holder.text = (TextView) convertView.findViewById(R.id.activeAlbumName);
+
+                // set the text and icon for the view
+                holder.text = (TextView)  convertView.findViewById(R.id.activeAlbumName);
                 holder.icon = (ImageView) convertView.findViewById(R.id.activeAlbumIcon);
-                convertView.setTag(ACTIVE_KEY,holder);
+
+                // set tag for this active view, containing the text and icon information
+                convertView.setTag(ACTIVE_KEY, holder);
             }
-            else{
+            // for the inactive position
+            else {
+                // inflate the view with the layout for "inactive position"
                 convertView = mInflater.inflate(R.layout.active_album,null,false);
-                holder.text = (TextView) convertView.findViewById(R.id.inactiveAlbumName);
+
+                // set the text and icon for the view
+                holder.text = (TextView)  convertView.findViewById(R.id.inactiveAlbumName);
                 holder.icon = (ImageView) convertView.findViewById(R.id.inactiveAlbumIcon);
+
+                // set tag for this inactive view, containing the text and icon information
                 convertView.setTag(INACTIVE_KEY,holder);
             }
         }
+
+        // if the view is in the active position
         else if (position == mActivePosition){
+            // load the text and icon information for the active position
             holder = (ViewHolder) convertView.getTag(ACTIVE_KEY);
         }
+        // if the view is in any of the inactive positions
         else{
+            // load the text and icon information for the inactive position
             holder = (ViewHolder) convertView.getTag(INACTIVE_KEY);
         }
+
+        // set the text and icon information for the view, whether it is active or inactive position
         holder.text.setText(album.getName());
         holder.icon.setImageResource(R.drawable.ic_launcher);
+
         return convertView;
     }
 
+    /**
+     * Initialize member variables
+     * @param context
+     * @param albums
+     * @param activePosition
+     */
     private void init(Context context, List<File> albums, int activePosition){
-        mContext = context;
-        mAlbums = albums;
+        mContext        = context;
+        mAlbums         = albums;
         mActivePosition = activePosition;
-        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mInflater       = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 }
--- CantwellGallery/src/main/java/com/cantwellcode/cantwellgallery/MainActivity.java
+++ CantwellGallery/src/main/java/com/cantwellcode/cantwellgallery/MainActivity.java
@@ -20,14 +20,21 @@ import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+/**
+ * Main Activity that is first loaded when the application starts
+ */
 public class MainActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<Cursor>{
-    private static final String TAG = "CANTWELL_GALLERY";
-    private static final Uri URI = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-    private static final String[] PROJECTION = {""};
-    private static final String SELECTION = null;
-    private static final String[] SELECTION_ARGS = {};
-    private static final String SORT_ORDER = null;
-    private static final int MEDIA_FILE_LOADER = 0;
+
+    private static final String   TAG               = "CANTWELL_GALLERY";
+    private static final Uri      URI               = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+    private static final String[] PROJECTION        = {""};
+    private static final String   SELECTION         = null;
+    private static final String[] SELECTION_ARGS    = {};
+    private static final String   SORT_ORDER        = null;
+    private static final int      MEDIA_FILE_LOADER = 0;
+
+    private ListView  mQuickBar;
+    private Cursor    mCursor;
 
     @Override
     public Loader<Cursor> onCreateLoader(int loaderID, Bundle bundle) {
@@ -49,12 +56,11 @@ public class MainActivity extends FragmentActivity implements LoaderManager.Load
 
     }
 
-    private ListView mQuickBar;
-    private Cursor mCursor;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // inflate the main view with the main activity layout
         final View root = getLayoutInflater().inflate(R.layout.activity_main, null);
         setContentView(root);
 
