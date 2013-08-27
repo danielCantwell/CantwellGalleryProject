620e9b99ae43ab2a631f24d5770e1655af7db21f
Daniel Cantwell <dcantwel@usc.edu>
2013-08-27 15:27:48 -0700

    Added 3 Interfaces for drag and drop. Added QuickBarListView.
    The 3 interfaces may need some adjustments.
    QuickBarListView extends ListView so that we can use this as our list view, and override touch events.

---
+++ CantwellGallery/src/main/java/com/cantwellcode/cantwellgallery/DragListener.java
@@ -0,0 +1,34 @@
+package com.cantwellcode.cantwellgallery;
+
+import android.view.View;
+import android.widget.ListView;
+
+/**
+ * Created by Daniel on 8/24/13.
+ *
+ * Implement to handle an item being dragged
+ */
+public interface DragListener {
+
+    /**
+     * Called when a drag starts
+     * @param itemView - the view of the item to be dragged, i.e. the drag view
+     */
+    void onStartDrag(View itemView);
+
+    /**
+     * Called when a drag is to be performed
+     * @param x - horizontal coordinate of MotionEvent
+     * @param y - vertical coordinate of MotionEvent
+     * @param listView - the listView
+     */
+    void onDrag(int x, int y, ListView listView);
+
+    /**
+     * Called when a drag stops
+     * Any changes in onStartDrag need to be undone here
+     * so that the view can be used in the list again
+     * @param itemView - the view of the item to be dragged, i.i. the drag view
+     */
+    void onStopDrag(View itemView);
+}
